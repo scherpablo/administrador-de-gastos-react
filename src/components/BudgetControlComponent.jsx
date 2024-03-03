468740e40b430752e7fec68e59a99a3bb49e5204
scherpablo <scherpablo@gmail.com>
2024-03-03 10:34:21 -0300

    circular progress styles

--- src/components/BudgetControlComponent.jsx
+++ src/components/BudgetControlComponent.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 import { formatCurrency } from "../utils";
+
+const percentage = 66;
 
 const BudgetComponent = ({ spents, budget }) => {
   const [available, setAvailabe] = useState(0);
@@ -20,9 +24,9 @@ const BudgetComponent = ({ spents, budget }) => {
   return (
     <>
       <div className="flex justify-center w-full px-5">
-        <div className="bg-[#3c3c3c] w-[600px] h-[300px] rounded-lg p-10 shadow-lg shadow-[#c3c3c3] flex ">
-          <div className="text-center h-[220px] flex flex-col justify-center my-0 m-auto">
-            <h2>Gráfica Aca</h2>
+        <div className="bg-[#3c3c3c] w-[600px] h-[300px] rounded-lg p-10 shadow-lg shadow-[#c3c3c3] flex gap-5">
+          <div className="w-1/3 flex">
+            <CircularProgressbar value={percentage} text={`${percentage}%`} />
           </div>
           <div className="text-center h-[220px] flex flex-col justify-center my-0 m-auto gap-3">
             <h2 className="text-left text-2xl">
