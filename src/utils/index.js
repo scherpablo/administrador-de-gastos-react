543338aaab26c737f72b7ad356be7dfc67b30c53
scherpablo <scherpablo@gmail.com>
2024-03-04 00:44:36 -0300

    add categories icons

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,27 +5,30 @@ const categories = [
     label: "ahorro",
     image: "/icons/icono_ahorro.svg",
   },
+  { id: "2", value: "casa", label: "casa", image: "/icons/icono_casa.svg" },
+  { id: "3", value: "ccoche", label: "coche", image: "/icons/icono_coche.svg" },
   {
-    id: "2",
+    id: "4",
     value: "comida",
     label: "comida",
     image: "/icons/icono_comida.svg",
   },
-  { id: "3", value: "casa", label: "casa", image: "/icons/icono_casa.svg" },
   {
-    id: "4",
+    id: "5",
     value: "gastos",
     label: "gastos",
     image: "/icons/icono_gastos.svg",
   },
-  { id: "5", value: "ocio", label: "ocio", image: "/icons/icono_ocio.svg" },
-  { id: "6", value: "salud", label: "salud", image: "/icons/icono_salud.svg" },
+  { id: "6", value: "ocio", label: "ocio", image: "/icons/icono_ocio.svg" },
+  { id: "7", value: "salud", label: "salud", image: "/icons/icono_salud.svg" },
+  { id: "9", value: "servicios", label: "servicios", image: "/icons/icono_servicios.svg" },
   {
-    id: "7",
+    id: "9",
     value: "suscripciones",
     label: "suscripciones",
     image: "/icons/icono_suscripciones.svg",
   },
+  { id: "10", value: "tarjeta", label: "tarjeta", image: "/icons/icono_tarjeta.svg" },
 ];
 
 const idGenerator = () => {
